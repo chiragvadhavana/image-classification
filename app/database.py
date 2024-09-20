c0f06e4392d000bca7e15d4b8213364b0a8af35a
Chirag Vadhavana <chiragvadhavana.yt@gmail.com>
2024-09-20 23:55:18 +0530

    database_url

--- app/database.py
+++ app/database.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = os.getenv('DATABASE_URL')
+print(f"DATABASE_URL: {DATABASE_URL}")  #
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
